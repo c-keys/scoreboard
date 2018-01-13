4891aaf1a28375dea01139f7cdcdb3f29f36a4c2
jetpacktuxedo <ethan@madden.ninja>
2018-01-12 19:32:46 -0800

    added sqlite score logging

--- score_listener.py
+++ score_listener.py
@@ -1,3 +1,5 @@
+import sqlite3
+
 from flask import Flask, request
 
 from score import SetLights
@@ -19,5 +21,13 @@ def score():
         PREV_SCORE = score
         lights = SetLights()
         adj_score = lights.adjust_score(score, lights.max_score, lights.strip.numPixels())
+        score_to_db(score)
         lights.animate_score(score)
     return str(PREV_SCORE)
+
+def score_to_db(score):
+    conn = sqlite3.connect('/root/score.db')
+    c = conn.cursor()
+    c.execute("insert into score values ({}, datetime('now'))".format(score))
+    conn.commit()
+    conn.close()
